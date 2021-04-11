b232658c03921370ca3dc50d9f996610d31c43aa
Damjan Rems <damjan.rems@gmail.com>
2021-04-11 16:01:41 +0200

    Ruby deprecation.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :mongoid_store, { expire_after: 30.days }
+Rails.application.config.session_store :mongoid_store, expire_after: 30.days 
 
