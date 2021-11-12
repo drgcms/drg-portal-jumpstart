2bee2de090d726571f5ca058baa0f4854de63072
Rems <damjan.rems@ozs.si>
2021-11-12 12:58:16 +0100

    So CK Editor will work as expected.

--- template.rb
+++ template.rb
@@ -1,5 +1,5 @@
-require "fileutils"
-require "shellwords"
+require 'fileutils'
+require 'shellwords'
 
 #########################################################################
 # Copied from: https://github.com/mattbrictson/rails-template
@@ -10,14 +10,14 @@ require "shellwords"
 #########################################################################
 def add_template_repository_to_source_path
   if __FILE__ =~ %r{\Ahttps?://}
-    require "tmpdir"
-    source_paths.unshift(temp_dir = Dir.mktmpdir("jumpstart-"))
+    require 'tmpdir'
+    source_paths.unshift(temp_dir = Dir.mktmpdir('jumpstart-'))
     at_exit { FileUtils.remove_entry(temp_dir) }
     git clone: [
-      "--quiet",
-      "https://github.com/drgcms/drg-portal-jumpstart.git",
+      '--quiet',
+      'https://github.com/drgcms/drg-portal-jumpstart.git',
       temp_dir
-    ].map(&:shellescape).join(" ")
+    ].map(&:shellescape).join(' ')
 
     if (branch = __FILE__[%r{jumpstart/(.+)/template.rb}, 1])
       Dir.chdir(temp_dir) { git checkout: branch }
@@ -30,8 +30,8 @@ end
 #########################################################################
 # Reads data from terminal input
 #########################################################################
-def read_input(message, default='')
-  message.split("\n").each {|e| print "#{e} " }
+def read_input(message, default = '')
+  message.split("\n").each { |e| print "#{e} " }
   response = STDIN.gets.chomp
   response.blank? ? default : response
 end
@@ -41,19 +41,19 @@ end
 #########################################################################
 def set_application_name
   # Add Application Name to Config
-  environment "config.application_name = Rails.application.class.parent_name"
+  environment 'config.application_name = Rails.application.class.parent_name'
 
   # Announce the user where he can change the application name in the future.
-  puts "You can change application name inside: ./config/application.rb"
+  puts 'You can change application name inside: ./config/application.rb'
 end
 
 #########################################################################
 #
 #########################################################################
 def copy_templates
-  directory "app", force: true
-  directory "config", force: true
-  directory "lib", force: true
+  directory 'app', force: true
+  directory 'config', force: true
+  directory 'lib', force: true
 end
 
 #def add_webpack
@@ -64,41 +64,40 @@ end
 #
 #########################################################################
 def update_routes
-  insert_into_file "config/routes.rb",
-%Q[
+  insert_into_file 'config/routes.rb', %(
   root :to => 'portal#page'
   DrgCms.routes
 
   put '/portal/process_login'
-  post '/reports/diary' 
-  
+  post '/reports/diary'
+
   resources :init # remove after initial run
-  
+
   get '*path' => 'portal#page'
 
-],
-  after: "Rails.application.routes.draw do"
+),
+    after: 'Rails.application.routes.draw do'
 end
 
 #########################################################################
 #
 #########################################################################
 def update_initializers
-# assets.rb  
-  append_to_file "config/initializers/assets.rb", 
-    "Rails.application.config.assets.precompile += %w( cms.css cms.js)"  
+  # assets.rb
+  append_to_file 'config/initializers/assets.rb',
+                 'Rails.application.config.assets.precompile += %w( cms.css cms.js)'
 end
 
 #########################################################################
-# 
+#
 #########################################################################
 def add_stage
-# duplicate production.rb  
+  # duplicate production.rb
   begin
-  copy_file '../' + File.join(@app_path,"config/environments/production.rb"),
-            '../' + File.join(@app_path,"config/environments/stage.rb"), force: true
-  rescue Exception => e
-    p 'Error creating stage.rb file. If you are going to use staging environment just copy config/environments/production.rb to stage.rb!'
+    FileUtils.cp File.join(@app_path,'config/environments/production.rb'),
+                 File.join(@app_path,'config/environments/stage.rb')
+    rescue Exception => e
+      p 'Error creating stage.rb file. If you are going to use staging environment just copy config/environments/production.rb to stage.rb!'
   end
 end
 
@@ -106,29 +105,29 @@ end
 #
 #########################################################################
 def update_application
-# action cable is not required  
-  gsub_file "config/application.rb",
-    /require \"action_cable\/engine\"/,
-    '## require "action_cable/engine"'
-  
-# add default forms path
-  append_to_file "config/application.rb", "DrgCms.add_forms_path Rails.root.join('app/forms')"
+  # action cable is not required
+  gsub_file 'config/application.rb',
+            /require \"action_cable\/engine\"/,
+            '## require "action_cable/engine"'
+
+  # add default forms path
+  append_to_file 'config/application.rb', "DrgCms.add_forms_path Rails.root.join('app/forms')"
 end
 
 #########################################################################
 #
 #########################################################################
 def update_mongoid_setup
-# update database names  
-  gsub_file "config/mongoid.yml", /drgcms_/, "#{@app_name}_"
+  # update database names
+  gsub_file 'config/mongoid.yml', /drgcms_/, "#{@app_name}_"
 end
 
 #########################################################################
 #
 #########################################################################
 def update_layout
-# update database names  
-  gsub_file "app/views/layouts/application.html.erb", 'javascript_pack_tag', 'javascript_include_tag'
+  # update database names
+  gsub_file 'app/views/layouts/application.html.erb', 'javascript_pack_tag', 'javascript_include_tag'
 end
 
 
@@ -136,27 +135,36 @@ end
 #
 #########################################################################
 def copy_gemfile
-  copy_file "Gemfile", force: true 
+  copy_file 'Gemfile', force: true
+end
+
+#########################################################################
+#
+#########################################################################
+def copy_ck_config
+  copy_file 'public/files/ck_config.js', force: true
+  copy_file 'public/files/ck_css.css', force: true
+  # I am cheating here, but couldn't get these icons displayed in development
+  copy_file 'public/icons.png', force: true
 end
 
 #########################################################################
 #
 #########################################################################
 def copy_bundler_setup
-  directory ".bundle", force: true
+  directory '.bundle', force: true
 end
 
 #########################################################################
 #
 #########################################################################
 def update_gitignore
-  append_to_file ".gitignore", 
-%Q[
+  append_to_file '.gitignore', %(
 #added by DRG
 .gitignore
 /vendor/ruby
 /.bundle
-]
+)
 end
 
 #########################################################################
@@ -168,19 +176,20 @@ add_template_repository_to_source_path
 copy_templates
 copy_bundler_setup
 copy_gemfile
-#
+copy_ck_config
+
 update_gitignore
 update_mongoid_setup
 update_application
 update_initializers
 update_layout
 add_stage
-#
+
 after_bundle do
   begin
     git :init
-    git add: "."
-    git commit: %Q{ -m 'Initial commit' --quiet }
+    git add: '.'
+    git commit: %( -m 'Initial commit' --quiet )
     p 'Git initial commit created.'
   rescue Exception
     p 'Error creating Git repository!'
